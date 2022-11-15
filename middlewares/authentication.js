383bc4e2687612869f586f184ce42b95ca5fe015
DanielMiguelez <danielmiguelez1993@gmail.com>
2022-11-15 13:17:08 +0100

    bug fixed

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,11 +1,11 @@
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
-const { jwt_secret } = require("../config/keys.js");
+require("dotenv").config()
 
 const authentication = async (req, res, next) => {
   try {
     const token = req.headers.authorization;
-    const payload = jwt.verify(token, jwt_secret);
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findOne({ _id: payload._id, tokens: token });
     if (!user) {
       return res.status(401).send({ message: "No estas autorizado" });
