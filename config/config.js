20771da86c4403ccefddccca53e378d1dd7d4d0b
DanielMiguelez <danielmiguelez1993@gmail.com>
2022-11-15 13:05:43 +0100

    console.log added

--- config/config.js
+++ config/config.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config()
 const dbConnection = async () => {
   try {
+    console.log(process.env.MONGO_URI)
     await mongoose.connect(process.env.MONGO_URI);
 
     console.log("Base de datos conectada con éxito");
